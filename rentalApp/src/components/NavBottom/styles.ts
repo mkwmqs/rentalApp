35d5c454e9cd25aae20d4a064e63a1ea17559ff7
Diego Czajka <diego.czajka@gmail.com>
2024-01-29 13:50:16 -0300

    feat:navigation tab bottom

---
+++ rentalApp/src/components/NavBottom/styles.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: "relative",
+  },
+  bottomFixed: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "#f0f0f0",
+    paddingVertical: 36,
+    paddingHorizontal: 8,
+  },
+  navRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignContent: "center",
+  },
+  iconItem: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 12,
+  },
+});
