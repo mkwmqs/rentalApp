76fdd0a3316a2f41028f780a6708484235287b34
Bruno Gabriel <brunintsu416@gmail.com>
2024-01-29 22:57:25 -0300

    Feat: Atualização de Login

--- rentalApp/src/screens/SignIn/index.tsx
+++ rentalApp/src/screens/SignIn/index.tsx
@@ -1,44 +1,116 @@
-import { View, Text, Icon, InfoIcon, Button, Center } from 'native-base'
-import React from 'react'
-import { SafeAreaView, TouchableOpacity, Image } from 'react-native'
-import { FontAwesome } from '@expo/vector-icons';
-import color from '../../styles/color';
-import { ParamListBase, useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
+import React, { useState } from 'react';
+import { Center, Divider, Radio, Stack } from 'native-base';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome'; 
+import { InfoBottom } from '../../components/InfoBottom';
 import { ColoredButton } from '../../components/ColoredButton';
+import color from '../../styles/color';
 
 
+export const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-export function SignIn() {
-  const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
-  function handleReturn() {
-    navigation.navigate('Welcome')
-  }
+  const handleAppleLogin = () => {
+    
+  };
+
+  const handleGoogleLogin = () => {
+   
+  };
+
+  const handleFacebookLogin = () => {
+    
+  };
+
+ 
 
   return (
     <>
-    
-      <View mt='16' ml='8'>
-        <TouchableOpacity onPress={handleReturn}>
-          <Icon as={FontAwesome} name='arrow-left' color={color.texting} />
-        </TouchableOpacity>
-      </View>
-
-      <Center mt='12'>
-        <Image source={require('../../../assets/ubinnerIcon.png')} alt="icone da ubinner" width={300} height={300} />
-      </Center>
-      <View mt='8' ml='8'>
-        <Text color={color.texting} fontSize='lg' fontWeight={'bold'}>Bem-vindo(a) a Bibipe</Text>
-      </View>
-
-      <View mt='8' p='4'>
-        <ColoredButton color={color.grey} title='Crie sua conta' />
-      </View>
-
-      <View pt='2' pl='4' pr='4'>
-        <ColoredButton color={color.light_blue} title='Já tenho uma conta' />
+    <View >
+      <View style={{gap:8, borderRadius:10, borderWidth: 1,borderColor: 'grey', padding:20, margin:20}}>
+      <Text >Email:</Text>
+      <TextInput
+        placeholder="Digite seu e-mail"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
       </View>
      
+
+      <View style={{gap:10, borderRadius:10, borderWidth: 1,borderColor: 'grey', padding:20, margin:20}}>
+      <Text>Senha:</Text>
+      <TextInput
+        placeholder="Digite sua senha"
+        secureTextEntry
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+      </View>
+
+      {/* <TouchableOpacity
+        style={{
+          borderRadius: 15,
+          borderWidth: 1,
+          borderColor: 'black',
+          padding: 10,
+          marginTop: 10,
+        }}
+        // onPress={handleEmailLogin}
+      >
+        
+      </TouchableOpacity> */}
+      <Text style={{textAlign: 'center',fontWeight: 'bold' }}>Enviaremos uma mensagem para você confirmando o seu número. Podem ser aplicadas tarifas padrões de dados e mensagens.</Text>
+
+      {/* <Center>
+      <Radio.Group name="exampleGroup" defaultValue="1" accessibilityLabel="pick a size">
+      <Stack direction={{
+      base: "column",
+      md: "row"
+    }} alignItems={{
+      base: "flex-start",
+      md: "center"
+    }} space={4} w="75%" maxW="300px">
+        <Radio value="1" colorScheme="red" size="sm" my={1}>
+          Small
+        </Radio>
+        <Radio value="2" colorScheme="green" size="md" my={1}>
+          Medium
+        </Radio>
+        <Radio value="3" colorScheme="yellow" size="lg" my={1}>
+          Large
+        </Radio>
+      </Stack>
+    </Radio.Group>
+    </Center> */}
+<View style={{padding: 8,marginTop: 4,}}>
+<ColoredButton color={color.light_blue} title='Continuar' />
+</View>
+
+      <Divider mt={20}  style={{ width: '100%' }} />
+      <TouchableOpacity style={{borderRadius:16, borderWidth: 1,borderColor: 'grey',padding: 12,marginTop: 12,}} onPress={handleAppleLogin}>
+        <Text>
+          <Icon name="apple" /> <Text style={{fontWeight: 'bold'}}>Continuar com Apple</Text>
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={{borderRadius:16, borderWidth: 1,borderColor: 'grey',padding: 12,marginTop: 12,}} onPress={handleGoogleLogin}>
+        <Text>
+          <Icon name="google" /> <Text style={{fontWeight: 'bold'}}>Continuar com Google</Text>
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={{borderRadius:16, borderWidth: 1,borderColor: 'grey',padding: 12,marginTop: 12,}} onPress={handleFacebookLogin}>
+        <Text>
+          <Icon name="facebook" /> <Text style={{fontWeight: 'bold',}}> Continuar com Facebook</Text>
+        </Text>
+      </TouchableOpacity>
+    </View>
+
+    <View style={{gap:8, padding:10, margin:10}}>
+    <InfoBottom/>
+    </View>
     </>
-  )
-}
+  );
+};
+
--- rentalApp/src/screens/Welcome/index.tsx
+++ rentalApp/src/screens/Welcome/index.tsx
@@ -1,46 +1,61 @@
+import { View, Text, Icon, InfoIcon, Button, Center } from 'native-base'
+import React from 'react'
+import { SafeAreaView, TouchableOpacity, Image } from 'react-native'
+import { FontAwesome } from '@expo/vector-icons';
+import color from '../../styles/color';
 import { ParamListBase, useNavigation } from '@react-navigation/native';
-import {StackNavigationProp} from '@react-navigation/stack'
-import React from 'react';
-import { Text, View } from 'react-native';
-
+import { StackNavigationProp } from '@react-navigation/stack';
 import { ColoredButton } from '../../components/ColoredButton';
 import { InfoBottom } from '../../components/InfoBottom';
-import color from '../../styles/color';
-import { styles } from './styles';
+import { NavBottom } from '../../components/NavBottom';
+
+
+
 
 export function Welcome() {
-  const navigator = useNavigation<StackNavigationProp<ParamListBase>>();
-  function handleCreateUser(): void {
-    navigator.navigate('CreateUser')
-  }
-  function handleLoginUser(){
-    navigator.navigate("SignIn")
-  }
+  
+  // const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
+  // function handleReturn() {
+  //   navigation.navigate('Welcome')
+    
+  // }
 
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.header}>Bem-vindo(a)</Text>
-      </View>
-      <View style={styles.content}>
-        <View style={styles.buttonWrapper}>
-          <ColoredButton
-            title='Crie sua conta'
-            color={color.grey}
-            onPress={handleCreateUser}
-          />
-        </View>
-        <View style={styles.buttonWrapper}>
-          <ColoredButton 
-          title='Login já sou cliente' 
-          color={color.light_blue} 
-          onPress={handleLoginUser}
-          />
-        </View>
+    <>
+   
+      <View mt='16' ml='8'>
+        <TouchableOpacity >
+          <Icon as={FontAwesome} name='arrow-left' color={color.texting} />
+        </TouchableOpacity>
       </View>
 
+      <Center mt='12'>
+        <Image source={require('../../../assets/ubinnerIcon.png')} alt="icone da ubinner" width={300} height={300} />
+      </Center>
+      <View mt='8' ml='8'>
+        <Text color={color.texting} fontSize='lg' fontWeight={'bold'}>Bem-vindo(a) a Bibipe</Text>
+      </View>
+
+      <View mt='8' p='4'>
+        <ColoredButton color={color.grey} title='Crie sua conta' />
+      </View>
+
+      <View pt='2' pl='4' pr='4'>
+        <ColoredButton color={color.light_blue} title='Já tenho uma conta' />
+      </View>
+      <Text style={{textAlign:'center', padding:8}}>Ao selecionar Continuar, você aceito os Termos de Serviços, os Termos de Serviços de Pagamentos, a Política de Não Discriminação e reconheço a Política de Privacidade. </Text>
+      <Text style={{textAlign: 'center'}}>Precisa de ajuda?</Text>
+
+      <View style={{gap:20, padding:20, margin:20}}>
       <InfoBottom />
-
-    </View>
+      
+      </View>
+      
+      <NavBottom/>
+    </>
   )
 }
+
+      
+
+   
